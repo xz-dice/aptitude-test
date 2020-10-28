17bfc47440d498c1b86962f9e87594c0c8ae72fa
Sarah <sairs_77@hotmail.com>
2020-10-28 16:59:09 +0000

    got data from the database logging to console

--- app/js/internalNotes.ts
+++ app/js/internalNotes.ts
@@ -5,12 +5,15 @@ function addNotesEventListeners() {
     document.querySelectorAll('.internal-notes-button').forEach((notesButton) => {
     notesButton.addEventListener('click', () => {
             let modal_content = document.querySelector("#modal-content");
-        getTemplateAjax('js/templates/internalNotesModal.hbs').then(function (HBTemplate) {
-            let template: Function = Handlebars.compile(HBTemplate);
-            let html = template();
-            modal_content.innerHTML += html
-        })
-            openDialog();
+            getData("result").then((data) => {
+                console.log(data);
+                getTemplateAjax('js/templates/internalNotesModal.hbs').then(function (HBTemplate) {
+                    let template: Function = Handlebars.compile(HBTemplate);
+                    let html = template();
+                    modal_content.innerHTML += html
+                })
+                openDialog();
+            })
         });
     })
 }
