0ca3dd5bbc8f13cc0d7fb7dfa930e76590931d40
Emil <loge3.14159@gmail.com>
2021-03-11 15:03:29 +0000

    import/export

--- app/js/convertTime.ts
+++ app/js/convertTime.ts
@@ -1,3 +1,6 @@
 function convertToTotalTimeSeconds(minutes: string, seconds: string): number {
     return ((parseInt(minutes) * 60) + parseInt(seconds));
 }
+export {
+    convertToTotalTimeSeconds
+}
