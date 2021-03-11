0dbf6ed6110253602ed1afc469f304c4dd2278c5
Emil <loge3.14159@gmail.com>
2021-03-11 17:05:10 +0000

    import/export

--- app/js/deleteQuestion.ts
+++ app/js/deleteQuestion.ts
@@ -3,6 +3,11 @@
  * @param {number} id the question id
  * @returns {Promise<*>} response from api
  */
+import {sendData} from "./sendData";
+
 async function deleteQuestion(id: number) {
     return await sendData(undefined, `/question/${id}/delete`);
+}
+export{
+    deleteQuestion
 }
