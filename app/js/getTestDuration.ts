31deff11701e4301b05ecf263a6ad4f192b6120f
Emil <loge3.14159@gmail.com>
2021-03-11 17:00:39 +0000

    import/export

--- app/js/getTestDuration.ts
+++ app/js/getTestDuration.ts
@@ -1,7 +1,12 @@
+import {getData} from "./getData";
+
 async function getTestDuration(test_id: number): Promise<number> {
     let test_fetch_result = await getData('test');
     let all_test_data = test_fetch_result.data;
     let selected_test_data = all_test_data[test_id - 1];
 
     return selected_test_data.time;
+}
+export{
+    getTestDuration
 }
